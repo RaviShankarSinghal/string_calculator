fe4a6e279c48d43866990b6cb04881ab42c23da6
ravi_dbspl <ravi@dbspl.co.in>
2024-12-19 15:17:05 +0530

    Add check for the , seperator string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,20 @@
+# frozen_string_literal: true
+
+# Returns the addition of the given string
 class StringCalculator
   def add(num_string)
     return 0 if num_string.empty?
-    return num_string.to_i if num_string.length == 1
 
+    numbers_array = split_numbers(num_string, ',')
+
+    numbers_array
+      .map(&:to_i)
+      .sum
+  end
+
+  private
+
+  def split_numbers(numbers_str, delimiter)
+    numbers_str.split(delimiter)
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,16 +1,31 @@
-require 'string_calculator'
+require_relative '../lib/string_calculator'
 
-RSpec.describe 'StringCalculator' do
-  let(:calculator) { StringCalculator.new }
+RSpec.describe StringCalculator do
+  subject(:calculator) { described_class.new }
 
   describe '#add' do
-    it 'returns 0 for an empty string' do
-      expect(calculator.add('')).to eq(0)
+    context 'with empty string' do
+      it 'returns 0' do
+        expect(calculator.add('')).to eq(0)
+      end
     end
 
-    it 'returns same value for a single length string' do
-      expect(calculator.add('1')).to eq(1)
-      expect(calculator.add('2')).to eq(2)
+    context 'with single number' do
+      it 'returns the number' do
+        expect(calculator.add('1')).to eq(1)
+      end
+    end
+
+    context 'with two numbers' do
+      it 'returns their sum' do
+        expect(calculator.add('1,2')).to eq(3)
+      end
+    end
+
+    context 'with unknown amount of numbers' do
+      it 'returns their sum' do
+        expect(calculator.add('1,2,3,4,5')).to eq(15)
+      end
     end
   end
 end
