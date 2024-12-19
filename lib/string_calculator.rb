bc154476f0959b3c92f2fa8e37ec9d135587344d
ravi_dbspl <ravi@dbspl.co.in>
2024-12-19 15:48:03 +0530

    Complete check the string with different delimiter

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,7 +5,10 @@ class StringCalculator
   def add(num_string)
     return 0 if num_string.empty?
 
-    numbers_array = split_numbers(num_string, ',')
+    delimiter, numbers_str = extract_delimiter(num_string)
+    return 0 if numbers_str.empty?
+
+    numbers_array = split_numbers(numbers_str, delimiter)
     validate_negatives!(numbers_array)
 
     numbers_array
@@ -25,5 +28,25 @@ class StringCalculator
     return unless negatives.any?
 
     raise "negatives not allowed: #{negatives.join(', ')}"
+  end
+
+  def extract_delimiter(input)
+    if input.start_with?('//')
+      delimiter_section = input[2..input.index("\n") - 1]
+      numbers = input[input.index("\n") + 1..]
+      delimiter = check_delimiter_section(delimiter_section)
+      [delimiter, numbers]
+    else
+      [/[,\n]/, input]
+    end
+  end
+
+  def check_delimiter_section(delimiter_section)
+    if delimiter_section.start_with?('[') && delimiter_section.end_with?(']')
+      delimiters = delimiter_section.scan(/\[(.*?)\]/).flatten
+      Regexp.union(delimiters)
+    else
+      delimiter_section
+    end
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -40,5 +40,35 @@ RSpec.describe StringCalculator do
         expect(calculator.add('2,1001')).to eq(2)
       end
     end
+
+    context 'with new lines between numbers' do
+      it 'returns their sum' do
+        expect(calculator.add("1\n2,3")).to eq(6)
+      end
+    end
+
+    context 'with custom delimiter' do
+      it 'returns sum using the specified delimiter' do
+        expect(calculator.add("//;\n1;2")).to eq(3)
+      end
+    end
+
+    context 'with delimiter of any length' do
+      it 'returns sum using the specified delimiter' do
+        expect(calculator.add("//[***]\n1***2***3")).to eq(6)
+      end
+    end
+
+    context 'with multiple delimiters' do
+      it 'returns sum using any of the specified delimiters' do
+        expect(calculator.add("//[*][%]\n1*2%3")).to eq(6)
+      end
+    end
+
+    context 'with multiple delimiters of varying lengths' do
+      it 'returns sum using any of the specified delimiters' do
+        expect(calculator.add("//[**][%%%]\n1**2%%%3")).to eq(6)
+      end
+    end
   end
 end
