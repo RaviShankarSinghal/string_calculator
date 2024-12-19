6062341e64f390cdd39d18cba483e5eb8b3b5d3d
ravi_dbspl <ravi@dbspl.co.in>
2024-12-19 13:09:38 +0530

    Add check for 1 length string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,7 @@
 class StringCalculator
   def add(num_string)
     return 0 if num_string.empty?
+    return num_string.to_i if num_string.length == 1
+
   end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -7,5 +7,10 @@ RSpec.describe 'StringCalculator' do
     it 'returns 0 for an empty string' do
       expect(calculator.add('')).to eq(0)
     end
+
+    it 'returns same value for a single length string' do
+      expect(calculator.add('1')).to eq(1)
+      expect(calculator.add('2')).to eq(2)
+    end
   end
 end
